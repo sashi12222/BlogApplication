557ffde7863e48d1aba6b46f735da6cfb87e069a
sashi12222 <sashiraj1222@gmail.com>
2024-11-15 14:29:18 +0530

    feat:blog application developed

---
+++ Frontend/src/components/GeneralScreens/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../Css/Footer.css'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer">
+            </div>
+            <div className="copyright">
+                <p className="copyright-blog">© 2024 CSE-B . All Rights Reserved <b> (Ravi,Mukesh,Priyanshu,Ujjwal,Kabish,Sashi) </b></p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
